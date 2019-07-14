d05cb1653a89f83f77dd545cc33473417dbff7af
fernando <rluis4490@gmail.com>
2019-07-14 15:55:36 -0600

    datatable

--- public/components/SurveyPie.js
+++ public/components/SurveyPie.js
@@ -3,23 +3,6 @@ class GenericTable extends ComponentGeneric {
     super(props);
   }
 
-  componentDidMount() {
-    $(document).ready(function () {
-      $('#students').DataTable();
-    });
-  }
-
-  sortByCol(colIndex) {
-    var feedSort = this.state.feed.sort((a, b) => {
-      a = a[colIndex];
-      b = b[colIndex];
-      return isNaN(a - b) ? a === b ? 0 : a < b ? -1 : 1 : a - b;
-    });
-    this.setState({
-      feed: feedSort
-    });
-  }
-
   renderTableHeaders() {
     const {
       labels
@@ -36,32 +19,24 @@ class GenericTable extends ComponentGeneric {
     }));
   }
 
-  renderTableData() {
-    const {
-      feed
-    } = this.state;
-    return feed.map((dimension, ky) => {
-      return React.createElement("tr", {
-        key: ky
-      }, dimension.map((element, index) => {
-        return React.createElement("td", {
-          key: index
-        }, element);
-      }));
-    });
-  }
-
   render() {
     return React.createElement("div", {
       className: "card",
       style: {
-        "maxWidth": "50rem"
+        "maxWidth": "20rem"
       }
     }, React.createElement("h4", {
       className: "card-title"
     }, this.state.title), React.createElement("div", {
       className: "card-header"
-    }), React.createElement("div", {
+    }, React.createElement("div", {
+      className: "md-form mt-0"
+    }, React.createElement("input", {
+      className: "form-control",
+      type: "text",
+      placeholder: "Search",
+      "aria-label": "Search"
+    }))), React.createElement("div", {
       className: "card-body"
     }, React.createElement("table", {
       className: "table table-condensed",
--- src/components/SurveyPie.js
+++ src/components/SurveyPie.js
@@ -1,28 +1,8 @@
-
-
 class GenericTable extends ComponentGeneric {
     constructor(props) {
       super(props);
     }
   
-   componentDidMount()
-   {
-      $(document).ready(function() {
-         $('#students').DataTable();
-     } );
-   }
-
-   sortByCol(colIndex){
-      var feedSort=this.state.feed.sort(
-         (a, b) =>{
-            a = a[colIndex];
-            b = b[colIndex];
-            return isNaN(a-b) ? (a === b) ? 0 : (a < b) ? -1 : 1 : a-b  ;
-         }
-      );
-      
-      this.setState({feed:feedSort});
-   }
    
    renderTableHeaders(){
         const { labels} = this.state;
@@ -37,30 +17,17 @@ class GenericTable extends ComponentGeneric {
            </tr>)
     }
 
-    renderTableData() {
-        const {feed} = this.state;  
-        
-        return feed.map((dimension,ky) => {
-           return <tr key={ky}>
-                 {
-                    dimension.map((element,index) =>{
-                       return (
-                          <td  key={index}>{element}</td>   
-                       )
-                    })
-                 }    
-           </tr>
-        })
-     }
-
+    
     render() {
         return (
            
-              <div className="card" style={{"maxWidth":"50rem"}}>
+              <div className="card" style={{"maxWidth":"20rem"}}>
                  <h4 className="card-title">{this.state.title}</h4>
   
                  <div className="card-header">
-                    
+                    <div className="md-form mt-0">
+                       <input className="form-control" type="text" placeholder="Search" aria-label="Search"/>
+                    </div>
                  </div>
                  <div className="card-body">
                     <table className="table table-condensed" id='students'>
